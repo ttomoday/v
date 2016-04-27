a6b06a87eb0f492539fc787ad459607018542fde
Vlad <ttomoday@gmail.com>
2016-04-27 20:57:48 +0300

    clear v repository

--- 2015/kms_booklet/www/js/main.js
+++
@@ -1,3 +0,0 @@
-$(function(){
-	$('a[rel=popover]').popover();
-})
--- 2016/rainway/js/selectbox/selectBoxConfig.js
+++
@@ -1,7 +0,0 @@
-$("select").each(function() {					
-	var sb = new SelectBox({
-		selectbox: $(this),
-		height: 240,
-		width: 550
-	});
-});
